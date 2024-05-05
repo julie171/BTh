4586c4f4661dfc920dfbc5c6cd3efc022701f59b
julie171 <pustynnaia@mail.ru>
2024-05-06 00:36:49 +0300

    adding and deleting comments

--- DijkstraAlgorithm.py
+++ DijkstraAlgorithm.py
@@ -1,156 +1,75 @@
-# import sys
-#
-#
-# class Graph(object):
-#     def __init__(self, nodes, init_graph):
-#         self.nodes = nodes
-#         self.graph = self.construct_graph(nodes, init_graph)
-#
-#     def construct_graph(self, nodes, init_graph):
-#         '''
-#         Этот метод обеспечивает симметричность графика: если существует путь от узла A к B со значением V, должен быть путь от узла B к узлу A со значением V.
-#         '''
-#         graph = {}
-#         for node in nodes:
-#             graph[node] = {}
-#
-#         graph.update(init_graph)
-#
-#         for node, edges in graph.items():
-#             for adjacent_node, value in edges.items():
-#                 if graph[adjacent_node].get(node, False) == False:
-#                     graph[adjacent_node][node] = value
-#
-#         return graph
-#
-#     def get_nodes(self):
-#         "Возвращает узлы графа"
-#         return self.nodes
-#
-#     def get_outgoing_edges(self, node):
-#         "Возвращает соседей узла"
-#         connections = []
-#         for out_node in self.nodes:
-#             if self.graph[node].get(out_node, False) != False:
-#                 connections.append(out_node)
-#         return connections
-#
-#     def value(self, node1, node2):
-#         "Возвращает значение ребра между двумя узлами."
-#         return self.graph[node1][node2]
-#
-#
-# def dijkstra_algorithm(graph, start, toZones, tauValues, links, idsLinks, idsZones):
-#     visitedNodes = list(len(graph.))
-#     shortest_path = {}
-#     previous_nodes = {}
-#     # Мы будем использовать max_value для инициализации значения "бесконечности" непосещенных узлов
-#     max_value = sys.maxsize
-#     for node in unvisited_nodes:
-#         shortest_path[node] = max_value
-#     # Однако мы инициализируем значение начального узла 0
-#     shortest_path[start_node] = 0
-#     while unvisited_nodes:
-#         current_min_node = None
-#         for node in unvisited_nodes:  # Iterate over the nodes
-#             if current_min_node == None:
-#                 current_min_node = node
-#             elif shortest_path[node] < shortest_path[current_min_node]:
-#                 current_min_node = node
-#                 neighbors = graph.get_outgoing_edges(current_min_node)
-#                 for neighbor in neighbors:
-#                     tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
-#                     if tentative_value < shortest_path[neighbor]:
-#                         shortest_path[neighbor] = tentative_value
-#                         # Мы также обновляем лучший путь к текущему узлу
-#                         previous_nodes[neighbor] = current_min_node
-#                         unvisited_nodes.remove(current_min_node)
-#     return previous_nodes, shortest_path
-
 import numpy as np
 import copy
 import heapq
 
 
-def dijkstra(graph, start, tauValues, links, idsLinks, idsZones):
-    # поиск подходящей вершины
+def dijkstra(graph, start, tauValues, links, idsZones):
+    # словарь для посещённых вершин
     visited = {vertex: False for vertex in graph}
+
+    # все зоны посещены, чтобы в них не заходить
     for i in idsZones:
         visited[i] = True
+
+    # инициализация
     dist = {vertex: np.inf for vertex in graph}
     path = {vertex: [] for vertex in graph}
     pq = []
     dist[start] = 0
+
+    # расстояния до всех вершин, соединенных коннекторами в начальной зоной, равны 0
     for i in graph[start]:
         dist[i] = 0
         heapq.heappush(pq, (0, i))
 
     while pq:
+        # ищем вершину с наименьшим расстоянием и которую еще не посещали
         currentDist, currentVertex = heapq.heappop(pq)
         while visited[currentVertex] == True:
             if len(pq) == 0:
                 return path, dist
             currentDist, currentVertex = heapq.heappop(pq)
 
+        # отмечаем, что вершина посещена
         visited[currentVertex] = True
+
+        # рассматриваем расстояния до соседей данной вершины
         for neighbor in graph[currentVertex]:
+            # пропускаем вершины, которые уже посетили
             if visited[neighbor] == True:
                 continue
+
+            # считаем расстояние до соседней вершины через данную
             index = links.index([currentVertex, neighbor])
             distance = currentDist + tauValues[index]
+
+            # если нашли меньшее расстояние, меняем путь и расстояние до вершины и записываем ее в вершины, которые нужно посетить
             if distance < dist[neighbor]:
                 dist[neighbor] = distance
                 path[neighbor].clear()
                 path[neighbor] = copy.deepcopy(path[currentVertex])
                 path[neighbor].append(index)
                 heapq.heappush(pq, (distance, neighbor))
-            # elif distance == dist[neighbor]:
-            #     way1 = copy.deepcopy(path[currentVertex])
-            #     way1.append(idsLinks[index])
-            #
-            #     if len(path[neighbor]) > 0 and isinstance(path[neighbor][0], list):
-            #         ways = []
-            #         for list1 in path[neighbor]:
-            #             ways.append(copy.deepcopy(list1))
-            #         path[neighbor].clear()
-            #         path[neighbor].extend(ways)
-            #     else:
-            #         ways = copy.deepcopy(path[neighbor])
-            #         path[neighbor].clear()
-            #         path[neighbor].append(ways)
-            #     path[neighbor].append(way1)
-
-        # distSorted = list(dist.values())
-        # distSorted.sort()
-        # found = False
-        # while len(distSorted) > 0 and found == False:
-        #     minDist = distSorted[0]
-        #     suitVerts = [key for key, value in dist.items() if value == minDist]
-        #     for vert in suitVerts:
-        #         if visited[vert] == False:
-        #             currentVertex = vert
-        #             found = True
-        #             break
-        #     while found == False and len(distSorted) > 0 and distSorted[0] == minDist:
-                # distSorted.pop(0)
-        # if distSorted[0] == np.inf:
-        #     return path, dist
 
     return path, dist
 
 
 def solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connectDict):
+    # инициализация переменных
     graph = graphNodesDict
     ye = np.array([0.0] * len(idsLinks))
     zonePath = []
     xp = []
+
     for id, fromZone in enumerate(idsZones):
+        # поиск зон, к которым кто-то хочет проехать из данной зоны
         toZones = [n for n, i in enumerate(rho[id]) if i > 0]
+
         if len(toZones) > 0:
-            # toNodes = []
-            # for k in toZones:
-            #     toNodes.append(graph[idsZones[k]])
-            path, dist = dijkstra(graph, fromZone, tauValues, links, idsLinks, idsZones)
+            # применяем алгоритм Дейкстры для данной подходящей зоны
+            path, dist = dijkstra(graph, fromZone, tauValues, links, idsZones)
+
+            # расписываем оптимальные пути из данной зоны в те, к которым кто-то хочет проехать, через вершины, соединенные коннекторами
             for toZone in toZones:
                 toNodes = connectDict[idsZones[toZone]]
                 minDist = np.inf
@@ -160,28 +79,17 @@ def solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connec
                         minDist = dist[node]
                         minNode = node
                 way = path[minNode]
+
+                # записываем путь с началом и концом в виде зон, которые этот путь соединяет
                 temp = copy.deepcopy(way)
                 temp.append(toZone)
                 zonePath.append([id] + temp)
-                # if isinstance(way[0], list):
-                #     for w in way:
-                #         for e in w:
-                #             index = np.where(idsLinks == e)
-                #             ye[index[0]] += rho[id][zone] / len(way)
-                # else:
+
+                # расписываем загруженность дорог, используя ограничения
                 for e in way:
                     ye[e] += rho[id][toZone]
+
+                # поток по пути
                 xp.append(rho[id][toZone])
 
     return zonePath, xp, ye
-
-
-# Define problem parameters
-# y_k = np.array([1, 2, 3])
-# grad_T = np.array([1, 1, 1])
-# A = np.array([[1, -1, 1], [1, 1, 1]])
-# b = np.array([3, 5])
-#
-# # Solve the optimization problem
-# minT_solution = solve_minT(y_k, grad_T, A, b)
-# print(minT_solution)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from numpy import *
+import numpy  as np
 from tau import Tau
 import DijkstraAlgorithm as dijkstra
 from reading import ReceivedDicts
@@ -7,62 +7,72 @@ from GoldenRatio import GoldenRatioSolver
 import copy
 
 
-def FrankWolf(idsZones, idsLinks, links, tauVal, graphNodesDict, rho, y0, gR, connectDict, epsilon, t):
+def FrankWolf(idsZones, idsLinks, links, tauVal, graphNodesDict, rho, y0, gR, connectDict, epsilon):
+    # инициализируем необходимые переменные
     gamma = 0.0
     k = 0
     yk = copy.deepcopy(y0)
-    #yArr.append(yk)
+
+    # линеаризируем функцию T
     diff, free = tauVal.LinearValue(yk)
+
+    # применяем алгоритм Дейкстры
     tauValues = tauVal.TauValues(yk)
-
     zonePath, xp, fk = dijkstra.solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connectDict)
-    s = sum(xp)
+
+    # строим направление
     pk = fk - yk
-    c = dot(diff, pk)
-    gamma = max(gamma, dot(diff, pk) + free)
 
-    while gamma == 0.0:
-        lk = gR.solve(0.00001, yk, pk)  # метод золотого сечения
-        yk = yk + lk * pk
-        k += 1
-
-        diff, free = tauVal.LinearValue(yk)
-        tauValues = tauVal.TauValues(yk)
-
-        zonePath, xp, fk = dijkstra.solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connectDict)
-        pk = fk - yk
-        c = dot(diff, pk)
-        gamma = max(gamma, dot(diff, pk) + free)
+    # ищем значение параметра для проверки критерия
+    gamma = max(gamma, np.dot(diff, pk) + free)
 
     while (b := (tauVal.TValue(yk) - gamma) / gamma) > epsilon:
-        lk = gR.solve(0.00001, yk, pk) # метод золотого сечения
+        # находим шаг с помощью метода золотого сечения
+        lk = gR.solve(0.00001, yk, pk)
         yk = yk + lk * pk
+
+        # вновь проверяем критерий
         if (tauVal.TValue(yk) - gamma) / gamma < epsilon:
             return yk, k
+
+        # меняем номер итерации
         k += 1
 
+        # вновь линеаризируем функцию T
         diff, free = tauVal.LinearValue(yk)
-        tauValues = tauVal.TauValues(yk)
 
+        # применяем алгоритм Дейкстры
+        tauValues = tauVal.TauValues(yk)
         zonePath, xp, fk = dijkstra.solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connectDict)
+
+        # строим направление
         pk = fk - yk
-        c = dot(diff, pk)
-        gamma = max(gamma, dot(diff, pk) + free)
+
+        # ищем значение параметра для проверки критерия
+        gamma = max(gamma, np.dot(diff, pk) + free)
+
     return yk, k, zonePath, xp
 
 
 def SolveTask():
-    # A = array([[1, 1]])  # eq
+    # получаем необходимые данные из файлов
     idsZones, idsLinks, links, c, t0, graphNodesDict, rho, connectDict = ReceivedDicts()
+
+    # инициализируем переменные
     tauVal = Tau(t0, c)
-    y0 = array([0.0] * len(idsLinks))
-    tauValues = tauVal.TauValues(y0)
-    _, _, y0 = dijkstra.solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connectDict)
+    y0 = np.array([0.0] * len(idsLinks))
     t = Task(t0, c)
     gR = GoldenRatioSolver(t)
-    epsilon = 0.01
+    epsilon = 0.001
 
-    FrankWolf(idsZones, idsLinks, links, tauVal, graphNodesDict, rho, y0, gR, connectDict, epsilon, tauValues)
+    # находим значения "весов" у ребер или время, которое придется потратить на проезд через каждую дорогу
+    tauValues = tauVal.TauValues(y0)
+
+    # применяем алгоритм Дейкстры с этими значениями
+    _, _, y0 = dijkstra.solve_minT(rho, tauValues, graphNodesDict, idsLinks, links, idsZones, connectDict)
+
+    # ищем решение задачи с помощью алгоритма Франка-Вульфа
+    yk, k, zonePath, xp = FrankWolf(idsZones, idsLinks, links, tauVal, graphNodesDict, rho, y0, gR, connectDict, epsilon)
 
 
 if __name__ == '__main__':
